5f555a8ef7d9bb0bf9d74c289a5cfc1120626907
takurou <i.taku45@gmail.com>
2018-07-06 15:13:12 +0900

    modify

--- post51/script1.js
+++ post51/script1.js
@@ -3,7 +3,7 @@ var device = 'pc';
 if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0) && (ua.indexOf('Mobile') > 0) || ua.indexOf('Windows Phone') > 0) {
   device = 'smt';
 }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
-  device = 'tab';
+  device = 'tablet';
 }
 
 $(function(){
